cede28576a5723ebcfaed4bf55f3d131caf58b9a
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-02-13 19:42:08 +0100

    Revert "composer require symfony/webpack-encore-bundle"
    This reverts commit b9f852fba065f4ab10b8e05f2c1793ed933376cd.

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
