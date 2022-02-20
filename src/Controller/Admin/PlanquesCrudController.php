fbc580af05bf5eab3338ee91a3d75808741415cb
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-02-20 19:16:37 +0100

    Clean imports

--- src/Controller/Admin/PlanquesCrudController.php
+++ src/Controller/Admin/PlanquesCrudController.php
@@ -7,7 +7,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class PlanquesCrudController extends AbstractCrudController
 {
