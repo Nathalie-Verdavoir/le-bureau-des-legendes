66cf95bb455341d4339381ebccff2c388cf485a4
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-02-22 18:33:53 +0100

    add Cruds

--- src/Controller/Admin/AgentsCrudController.php
+++ src/Controller/Admin/AgentsCrudController.php
@@ -5,9 +5,12 @@ namespace App\Controller\Admin;
 use App\Entity\Agents;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AgentsCrudController extends AbstractCrudController
 {
