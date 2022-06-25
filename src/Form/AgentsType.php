f5dcb2618281c79f89cd85e9584d621550ccbd88
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-06-25 23:04:04 +0200

    Fix duplicated code fix

--- src/Form/AgentsType.php
+++ src/Form/AgentsType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Agents;
+use App\Entity\Pays;
 use App\Entity\Specialites;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,9 +17,20 @@ class AgentsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contact', PersonType::class, [
-                'data_class' => Agents::class,
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('date_de_naissance', BirthdayType::class,[
+                'attr' => ['class' => 'inlineForm'],
             ])
+            ->add('nom_de_code', NomDeCodeAjouter::class, [
+                'attr' => ['class' => 'inlineForm'],
+                'label' => false
+            ])
+            ->add('nationalite', EntityType::class, array(
+                'class' => Pays::class,
+                'choice_label' => 'nom',              
+                'label' => 'Pays d\'origine'
+            ))
             ->add('specialites', EntityType::class, array(
                 'class' => Specialites::class,
                 'allow_extra_fields' => true,
--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Pays;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
